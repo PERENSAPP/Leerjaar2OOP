6ea8967bb4f07ebf90df683d4e1214be4065d4de
PERENSAPP <119853517+PERENSAPP@users.noreply.github.com>
2024-03-15 16:29:59 +0100

    Deels klaar met de Database class. voor nu

--- school.php
+++ school.php
@@ -51,4 +51,25 @@ echo $nieuweTrein->getSoort();
 
 // }
 /////////////////////////////////////////////////////////////////
+
+/////// HEEL BELANGRIJK 
+class Database
+{
+    public $connection;
+
+    public function __construct()
+    {
+        $dsn = "mysql:host=localhost;dbname=;user=root;";
+        $this->connection = new PDO($dsn);
+    }
+    public function query($query)
+    {
+        $statement = $this->connection->prepare($query);
+        $statement->execute();
+
+        return $statement->fetchAll();
+    }
+}
+
 ?>
+
