c464c762bc0682db24f445bc3e06542b7b5191e2
KEVN <troostkevin8@gmail.com>
2024-03-25 09:56:27 +0100

    login complete

--- loginLogic.php
+++ loginLogic.php
@@ -2,38 +2,48 @@
 session_start();
 require_once "conn.php";
 
+class LoginLogic {
+    private $conn;
+
+    public function __construct($conn) {
+        $this->conn = $conn;
+    }
+
+    public function login($email, $password) {
+        $stmt = $this->conn->prepare("SELECT * FROM account WHERE email = :un");
+        $stmt->execute(["un" => $email]);
+        $data = $stmt->fetch(PDO::FETCH_OBJ);
+
+        if (!$data) {
+            // User is not found
+            header("location: retry_login.php"); // fix this
+            exit;
+        }
+
+        $hashed_password = $data->password;
+        $username = $data->username;
+        $email = $data->email;
+        $id = $data->id;
+
+        if (password_verify($password, $hashed_password)) {
+            // If the password is correct 
+            // $_SESSION["logged_in_user"] = $username;
+            // $_SESSION["username"] = $username;
+            // $_SESSION["id"] = $id;
+            // $_SESSION["email"] = $email;
+            header("location: logged_in_user.php"); // fix this
+            exit;
+        } else {
+            // This is if the typed in password is incorrect
+            header("location: retry_login.php"); // fix this
+            exit;
+        }
+    }
+}
+
 $email = strip_tags($_POST["email"]);
 $password = strip_tags($_POST["password"]);
 
-// gets the hashed password from the table account 
-$sql = "SELECT * FROM account WHERE email = :un";
-$stmt = $conn->prepare($sql);
-$stmt->execute(["un" => $email]);
-$data = $stmt->fetch(PDO::FETCH_OBJ);
-
-$hashed_password = $data->password;
-$username = $data->username;
-$email = $data->email;
-$id = $data->id;
-
-//----------------------------------------------------- 
-
-if (!$data) {
-    // User is not found
-    header("location: retry_login.php");
-    exit;
-}
-if (password_verify($password, $hashed_password)) {
-    // If the password is correct 
-    $_SESSION["logged_in_user"] = $username;
-    $_SESSION["username"] = $username;
-    $_SESSION["id"] = $id;
-    $_SESSION["email"] = $email;
-    header("location: logged_in_user.php");
-    exit;
-} else {
-    // This is if the typed in password is incorrect
-    header("location: retry_login.php");
-    exit;
-}
+$loginLogic = new LoginLogic($conn);
+$loginLogic->login($email, $password);
 ?>
