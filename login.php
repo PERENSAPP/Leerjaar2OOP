964e70fb4117bdfe2f962f05d2b902d3e932278f
KEVN <troostkevin8@gmail.com>
2024-03-26 08:38:08 +0100

    Update login.php

--- login.php
+++ login.php
@@ -31,7 +31,7 @@
         <div class="container-fluid h-custom">
             <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col-md-9 col-lg-6 col-xl-5">
-                    <img src="Pictures/school1.png" width="393" height="393" class="img-fluid" alt="Sample image">
+                    <img src="Pictures/school1.png" width="393" height="393" class="img-fluid" alt="">
                 </div>
                 <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
                     <form>
